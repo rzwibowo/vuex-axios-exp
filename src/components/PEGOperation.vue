6b9a601e69fc29b31467dc987acfb9872d766fbe
rzwibowo <d7revenge@gmail.com>
2019-09-17 10:18:44 +0700

    ubah dispatch ke mapactions

--- src/components/PEGOperation.vue
+++ src/components/PEGOperation.vue
@@ -1,5 +1,22 @@
 <template>
     <div>
-        <button @click="$store.dispatch('loadPegawai')">get data</button>
+        <!-- without mapActions -->
+        <!-- <button @click="$store.dispatch('loadPegawai')">get data</button> -->
+
+        <!-- with mapActions -->
+        <button @click="loadPegawai">get data</button>
     </div>
 </template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+    name: 'OpPeg',
+    methods: {
+        ...mapActions([
+            'loadPegawai'
+        ])
+    }
+}
+</script>
